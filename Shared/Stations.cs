ba9211bfb574bcc9425e033d5acbe9c399e81889
Simo Hämäläinen <76773814+Hovari@users.noreply.github.com>
2023-03-27 12:56:42 +0300

    Classes for database

---
+++ Shared/Stations.cs
@@ -0,0 +1,19 @@
+﻿namespace HelsinkiCityBikeApp.Shared
+{
+    public class Station
+    {
+        public int FID { get; set; }
+        public int ID { get; set; }
+        public string? Nimi { get; set; }
+        public string? Namn { get; set; }
+        public string? Name { get; set; }
+        public string? Osoite { get; set; }
+        public string? Adress { get; set; }
+        public string? Kaupunki { get; set; }
+        public string? Stad { get; set; }
+        public string? Operaattor { get; set; }
+        public int? Kapasiteet { get; set; }
+        public double X { get; set; }
+        public double Y { get; set; }
+    }
+}
---
+++ Shared/Trips.cs
@@ -0,0 +1,15 @@
+﻿namespace HelsinkiCityBikeApp.Shared
+{
+    public class Trips
+    {
+        public int ID { get; set; }
+        public string? Departure { get; set; }
+        public string? Return { get; set; }
+        public int? DepartureStationId { get; set; }
+        public string? DepartureStationName { get; set; }
+        public int? ReturnStationId { get; set; }
+        public string? ReturnStationName { get; set; }
+        public int? CoveredDistance { get; set; }
+        public int? Duration { get; set; }
+    }
+}
