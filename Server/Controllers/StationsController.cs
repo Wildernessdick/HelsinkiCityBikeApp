918c2a57576f8a7057100c28073e9a38843d1c1f
Simo Hämäläinen <76773814+Hovari@users.noreply.github.com>
2023-03-27 13:19:53 +0300

    Controllers added for stations and trips

---
+++ Server/Controllers/StationsController.cs
@@ -0,0 +1,67 @@
+﻿using HelsinkiCityBikeApp.Server.Data;
+using HelsinkiCityBikeApp.Shared;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelsinkiCityBikeApp.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StationsController : ControllerBase
+    {
+        private readonly HelsinkiCityBikeContext _context;
+        public StationsController(HelsinkiCityBikeContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public IActionResult GetStations()
+        {
+            return Ok(_context.Stations);
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetStation(int id)
+        {
+            var station = _context.Stations.Find(id);
+            if (station == null)
+            {
+                return NotFound();
+            }
+            return Ok(station);
+        }
+        [HttpPost]
+        public IActionResult PostStation(Station station)
+        {
+            _context.Stations.Add(station);
+            _context.SaveChanges();
+            return StatusCode(StatusCodes.Status201Created);
+        }
+        [HttpPut("{id}")]
+        public IActionResult PutStation(int id, Station station)
+        {
+            if (id != station.ID)
+            {
+                return BadRequest();
+            }
+            _context.Entry(station).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+            _context.SaveChanges();
+            return NoContent();
+        }
+        [HttpDelete("{id}")]
+        public IActionResult DeleteStation(int id)
+        {
+            var station = _context.Stations.Find(id);
+            if (station == null)
+            {
+                return NotFound();
+            }
+            _context.Stations.Remove(station);
+            _context.SaveChanges();
+            return NoContent();
+        }
+        private bool StationExists(int id)
+        {
+            return _context.Stations.Any(e => e.ID == id);
+        }
+
+    }
+}
---
+++ Server/Controllers/TripsController.cs
@@ -0,0 +1,81 @@
+﻿using HelsinkiCityBikeApp.Server.Data;
+using HelsinkiCityBikeApp.Shared;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+namespace HelsinkiCityBikeApp.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TripsController : ControllerBase
+    {
+        private readonly HelsinkiCityBikeContext _context;
+        public TripsController(HelsinkiCityBikeContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Trips>>> GetTrips()
+        {
+            return await _context.Trips.ToListAsync();
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Trips>> GetTrip(int id)
+        {
+            var trip = await _context.Trips.FindAsync(id);
+            if (trip == null)
+            {
+                return NotFound();
+            }
+            return trip;
+        }
+        [HttpPost]
+        public async Task<ActionResult<Trips>> PostTrip(Trips trip)
+        {
+            _context.Trips.Add(trip);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction("GetTrip", new { id = trip.ID }, trip);
+        }
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTrip(int id, Trips trip)
+        {
+            if (id != trip.ID)
+            {
+                return BadRequest();
+            }
+            _context.Entry(trip).State = EntityState.Modified;
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TripExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return NoContent();
+        }
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Trips>> DeleteTrip(int id)
+        {
+            var trip = await _context.Trips.FindAsync(id);
+            if (trip == null)
+            {
+                return NotFound();
+            }
+            _context.Trips.Remove(trip);
+            await _context.SaveChangesAsync();
+            return trip;
+        }
+        private bool TripExists(int id)
+        {
+            return _context.Trips.Any(e => e.ID == id);
+        }
+
+    }
+}
